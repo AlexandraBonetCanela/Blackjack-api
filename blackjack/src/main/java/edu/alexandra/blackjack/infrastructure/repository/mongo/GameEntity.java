3ddb7870dddad7741017384ccc4676ed64a8f148
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-01-28 17:00:23 +0100

    game mapper

--- blackjack/src/main/java/edu/alexandra/blackjack/infrastructure/repository/mongo/GameEntity.java
+++ blackjack/src/main/java/edu/alexandra/blackjack/infrastructure/repository/mongo/GameEntity.java
@@ -1,5 +1,6 @@
 package edu.alexandra.blackjack.infrastructure.repository.mongo;
 
+import edu.alexandra.blackjack.domain.Game;
 import edu.alexandra.blackjack.domain.GameStatus;
 import edu.alexandra.blackjack.domain.Player;
 import lombok.*;
@@ -23,4 +24,20 @@ public class GameEntity {
     private Player player;
 
     private GameStatus status;
+
+    public static Game toDomain(GameEntity gameEntity) {
+        return Game.builder()
+                .id(gameEntity.getId() != null? Long.valueOf(gameEntity.getId()) : null)
+                .status(gameEntity.getStatus())
+                .player(gameEntity.getPlayer())
+                .build();
+    }
+
+    public static GameEntity toEntity(Game game){
+        return GameEntity.builder()
+                .id(game.getId() != null? game.getId().toString() : null)
+                .status(game.getStatus())
+                .player(game.getPlayer())
+                .build();
+    }
 }
