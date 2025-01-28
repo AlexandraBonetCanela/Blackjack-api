008f05c3fd03e45e53b2f2b09ea15c0a3eaa0f5b
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-01-28 14:59:09 +0100

    added Player DTO

---
+++ blackjack/src/main/java/edu/alexandra/blackjack/domain/Player.java
@@ -0,0 +1,10 @@
+package edu.alexandra.blackjack.domain;
+
+import java.math.BigDecimal;
+
+public class Player {
+
+    private Long id;
+    private String playerName;
+    private BigDecimal totalScore;
+}
