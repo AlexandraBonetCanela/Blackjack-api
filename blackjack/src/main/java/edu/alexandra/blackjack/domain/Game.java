97bd0400212f9b11ba157e4e9518e27d7b97a535
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-01-28 10:58:23 +0100

    added Mongo and Mysql dependencies

--- blackjack/src/main/java/edu/alexandra/blackjack/domain/Game.java
+++ blackjack/src/main/java/edu/alexandra/blackjack/domain/Game.java
@@ -1,5 +1,6 @@
 package edu.alexandra.blackjack.application.rest.response;
 
+import edu.alexandra.blackjack.domain.Status;
 import lombok.*;
 
 @Getter
@@ -12,4 +13,5 @@ public class GetGameResponse {
 
     private final Long id;
     private final String playerName;
+    private final Status status;
 }
