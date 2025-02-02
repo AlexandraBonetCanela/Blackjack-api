d8f9c30aa08361909e9bc23ec31716c232aef30e
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2025-02-02 21:51:44 +0100

    added Game logic

--- blackjack/src/main/java/edu/alexandra/blackjack/domain/Game.java
+++ blackjack/src/main/java/edu/alexandra/blackjack/domain/Game.java
@@ -2,6 +2,7 @@ package edu.alexandra.blackjack.domain;
 
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -12,38 +13,131 @@ import java.util.UUID;
 @Setter
 @ToString
 @EqualsAndHashCode
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
 public class Game {
 
     private final UUID id;
     private final Player player;
-    private Deck deck;
-    private List<Card> playerHand;
-    private List<Card> dealerHand;
+
+    @Builder.Default
+    private Deck deck = new Deck();
+
+    @Builder.Default
+    private List<Card> playerHand = new ArrayList<>();
+
+    @Builder.Default
+    private List<Card> dealerHand = new ArrayList<>();
+
     private final BigDecimal moneyBet;
     private GameStatus status;
+    private GameResult gameResult;
 
-    public void dealInitialCards(){
-
-        if (this.deck == null){
-            this.deck = new Deck();
-        }
-
-        if (this.dealerHand == null){
-            this.dealerHand = new ArrayList<>();
-        }
-
-        if (this.playerHand == null){
-            this.playerHand = new ArrayList<>();
-        }
+    public Game dealInitialCards(){
 
         if (deck.getCards().size() >= 4) {
-            playerHand.add(deck.getCard());
-            dealerHand.add(deck.getCard());
-            playerHand.add(deck.getCard());
-            dealerHand.add(deck.getCard());
+            return this.toBuilder()
+                    .playerHand(List.of(deck.getCard(), deck.getCard()))
+                    .dealerHand(List.of(deck.getCard(), deck.getCard()))
+                    .build();
         }
+        return this;
+    }
+
+    public static int getCardScore(Card card){
+
+        return switch (card.getRank()) {
+            case "Jack", "Queen", "King" -> 10;
+            case "ACE" -> 1;
+            default -> Integer.parseInt(card.getRank());
+        };
+    }
+
+    public static int getHandScore(List<Card> hand) {
+
+        boolean aceIsPresent = false;
+        int total = 0;
+
+        for (Card card: hand){
+            if (card.getRank().equals("ACE")) {
+                aceIsPresent = true;
+            }
+            total += getCardScore(card);
+        }
+
+        if (aceIsPresent && ((total + 10) <= 21)) {
+                total += 10;
+        }
+        return total;
+    }
+
+    public Mono<Game> executeGameLogic(MoveType move){
+
+        // higher than the dealer's
+        // Max 21
+
+        switch (move){
+            case MoveType.STAND :
+                stand();
+                break;
+            case MoveType.HIT:
+                hit();
+                break;
+        }
+        return Mono.just(this);
+    }
+
+    public void stand(){
+
+        int playerScore = getHandScore(playerHand);
+        int dealerScore = getHandScore(dealerHand);
+
+        if (playerScore > dealerScore) {
+            gameResult = GameResult.WON;
+        } else if (playerScore < dealerScore) {
+            gameResult = GameResult.LOST;
+        } else {
+            gameResult = GameResult.DRAW;
+        }
+        finishGame();
+    }
+
+    public void hit(){
+
+        playerHand.add(deck.getCard());
+
+        int playerScore = getHandScore(playerHand);
+        if (playerScore == 21 ){
+            gameResult = GameResult.WON;
+            finishGame();
+            return;
+        }
+        if (playerScore > 21) {
+            gameResult = GameResult.BUST;
+            finishGame();
+            return;
+        }
+
+        int dealerScore = getHandScore(dealerHand);
+        if (dealerScore < 17) {
+            dealerHand.add(deck.getCard());
+            dealerScore = getHandScore(dealerHand);
+
+            if (dealerScore == 21) {
+                gameResult = GameResult.LOST;
+                finishGame();
+                return;
+            }
+            
+            if (dealerScore > 21) {
+                gameResult = GameResult.WON;
+                finishGame();
+            }
+        }
+    }
+
+    public void finishGame(){
+        status = GameStatus.FINISHED;
     }
 }
